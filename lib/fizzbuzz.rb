4014a3fa8255b3753358d3eba45e05225e206204
Saif-ul Hussain <shssn8@gmail.com>
2022-05-11 11:11:39 +0000

    Working Method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,13 @@
-require 'fizzbuzz'
+#require 'fizzbuzz'
 
 def fizzbuzz(number)
-    "fizz"
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  elsif number % 3 == 0
+    return "fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  else
+    return number
+  end
 end
