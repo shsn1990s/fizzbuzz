a2ba23ea1948cc4d2342b970401683cf3f5b7b6d
Saif-ul Hussain <shssn8@gmail.com>
2022-05-11 10:40:05 +0000

    Created Lib Folder

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,18 @@
-puts "Please enter a number"
-number = gets.chomp.to_i
+# puts "Please enter a number"
+# number = gets.chomp.to_i
 counter = 1
 
-while counter <= number do
-  if counter % 3 == 0 && counter % 5 == 0
+def fizzbuzz(number)
+#while counter <= 20 do
+  if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
-  elsif counter % 3 == 0
-    puts "Fizz"
-  elsif counter % 5 == 0
-    puts "Buzz"
+  elsif number % 3 == 0
+    puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
   else
-    counter
+    number
   end
-  counter += 1
+  #counter += 1
+#end
 end
-    
